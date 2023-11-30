7259caa46e03e6e83b4802b25573871414d87348
fabrialberio <->
2023-11-30 20:49:27 +0100

    Add filter widgets to view_lessons.php

---
+++ views/filter_widgets.php
@@ -0,0 +1,34 @@
+<?php
+function generate_before_filters_bar() {
+    echo "
+    <div class='mb-2'>
+        <form action='view_lessons.php' method='get'>
+            <div class='input-group'>";
+}
+
+function generate_filter_select(string $name, string $default_string, array $options, string $selected = null) {
+    echo "<select class='form-control' name='$name'>
+        <option value=''>$default_string</option>";
+
+    foreach ($options as $option) {
+        $value = $option[0];
+        $text = $option[1];
+        $selected_attr = $selected == $value ? 'selected' : '';
+        echo "<option value='$value' $selected_attr>$text</option>";
+    }
+
+    echo "</select>";
+}
+
+function generate_after_filters_bar() {
+    echo "
+                <div class='input-group-append'>
+                    <button class='btn btn-primary' type='submit'>
+                        <i class='fas fa-filter'></i>
+                        Filtra
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>";
+}
--- views/view_lessons.php
+++ views/view_lessons.php
@@ -3,6 +3,7 @@ include_once '../database/interface.php';
 include_once '../src/token.php';
 include_once '../src/navigation.php';
 include_once '../views/view_lesson_widgets.php';
+include_once '../views/filter_widgets.php';
 
 session_start();
 $token = decode_token_or_quit($_SESSION['token']);
@@ -36,18 +37,52 @@ $amministratore = is_amministratore_by_username($token['username']);
     <section class="content pb-5">
       <?php
         $id_docente_get = strlen($_GET['id_docente']) ? intval($_GET['id_docente']) : null;
+        $eliminate_get = strlen($_GET['eliminate']) ? boolval($_GET['eliminate']) : FALSE;
         $id_classe_get = strlen($_GET['id_classe']) ? intval($_GET['id_classe']) : null;
 
         $id_docente = $amministratore ? $id_docente_get : get_id_docente_by_username($token['username']);
+        $eliminate = $amministratore ? $eliminate_get : FALSE;
         $id_classe = $id_classe_get;
+
 
         $lezioni = get_lezioni_filter(
           id_docente: $id_docente,
           id_classe: $id_classe,
-          eliminata: FALSE,
+          eliminata: $eliminate,
         );
         
-        generate_filters_bar($amministratore);
+        generate_before_filters_bar();
+
+        if ($amministratore) {
+            $docenti_options = array_map(
+                function($d) {
+                    return [
+                        0 => $d['id'],
+                        1 => $d['cognome_nome'],
+                    ];
+                },
+                get_docenti()
+            );
+            generate_filter_select('id_docente', 'Tutti i docenti', $docenti_options, $_GET['id_docente'] ?? null);
+
+            $eliminata_options = [
+              0 => [TRUE, 'Eliminate'],
+            ];
+            generate_filter_select('eliminate', 'Non eliminate', $eliminata_options, $_GET['eliminate'] ?? null);
+          }
+    
+        $classi_options = array_map(
+            function($c) {
+                return [
+                    0 => $c['id'],
+                    1 => $c['classe'],
+                ];
+            },
+            get_classi()
+        );
+        generate_filter_select('id_classe', 'Tutte le classi', $classi_options, $_GET['id_classe'] ?? null);
+
+        generate_after_filters_bar();
 
         if (count($lezioni) == 0) {
           echo "
