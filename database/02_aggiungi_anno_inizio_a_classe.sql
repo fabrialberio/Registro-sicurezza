95d0ac734dc29cb89c4174a5646c709b7bd1b941
fabrialberio <->
2025-09-16 08:33:22 +0200

    feat: Remove `classe.anno` and merge it with `classe.sezione`

--- database/02_aggiungi_anno_inizio_a_classe.sql
+++ database/02_aggiungi_anno_inizio_a_classe.sql
@@ -1,2 +1,5 @@
 ALTER TABLE classe ADD COLUMN anno_inizio INT NOT NULL;
 UPDATE classe SET anno_inizio = 2024;
+
+UPDATE classe SET sezione = CONCAT(anno, sezione);
+ALTER TABLE classe DROP COLUMN anno;
