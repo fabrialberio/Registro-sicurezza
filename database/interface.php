2ee749f53f1b8e22c028660b477b0671370eb4a1
fabrialberio <->
2025-09-16 08:33:59 +0200

    feat: Implement `classe.anno_inizio`

--- database/interface.php
+++ database/interface.php
@@ -36,9 +36,9 @@ function get_classi(): array {
     
     $query = mysqli_query(
         $connection,
-        "SELECT id, CONCAT(anno, sezione) AS classe
+        "SELECT id, CONCAT(sezione, ' ', anno_inizio, '-', RIGHT(anno_inizio + 1, 2)) AS sezione_anno, sezione, anno_inizio
         FROM classe
-        ORDER BY anno, sezione"
+        ORDER BY anno_inizio DESC, sezione"
     );
     return mysqli_fetch_all($query, MYSQLI_BOTH);
 }
@@ -48,7 +48,7 @@ function get_classe(int $id_classe): array {
     
     $query = mysqli_query(
         $connection,
-        "SELECT id, CONCAT(anno, sezione)
+        "SELECT id, CONCAT(sezione, ' ', anno_inizio, '-', RIGHT(anno_inizio + 1, 2)) AS sezione_anno, sezione, anno_inizio
         FROM classe
         WHERE id =" . strval($id_classe)
     );
